bb64e852460105e55b4c151085189b2d5eace63b
jeniferplacido <jenifer.sdti@gmail.com>
2021-05-28 18:08:24 -0300

    Exercicio2

---
+++ Exercicio2.js
@@ -0,0 +1,7 @@
+// 2 - crie um algoritmo que converte dias em horas, 
+// quando recebe um número de dias.
+function DiaEmHoras(dias) {
+    const horas = dias * 24;
+    return `${horas} horas`;
+  }
+  console.log(DiaEmHoras(30))
